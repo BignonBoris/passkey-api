0d9c0356aae825b3471173d2c116af7236561d66
Bignon Boris <bignonboris@gmail.com>
2026-05-02 08:18:45 +0100

    update

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,2 +1,5 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "super_secret_key";
 export const JWT_EXPIRES_IN = "7d"; // ou "1h", selon besoin
+
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "super_refresh_secret_key";
+export const JWT_REFRESH_EXPIRES_IN = "365d"; 
